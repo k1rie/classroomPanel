452e70893b72dd1c8659b46ebf23f97501ee4d3b
k1rie <88909577+k1rie@users.noreply.github.com>
2024-10-08 13:24:20 -0600

    Update Attendance.jsx

--- src/Attendance.jsx
+++ src/Attendance.jsx
@@ -1,10 +1,25 @@
 import { useParams } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import attendanceStyles from "./styles/attendance.module.css"
+import { useEffect } from "react"
 
 const Attendance = ()=>{
 
-    const {lastname,name,grade,group,area} = useParams()
+    const {lastname,name,grade,group,area,email} = useParams()
+
+    const attendance = ()=>{
+        fetch(`https://tasksflow-backend.onrender.com/attendance/${name}/${lastname}/${grade}/${group}/${area}/${email}`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+          
+        })
+    }
+
+    useEffect(()=>{
+        attendance()
+    },[])
 
     return(
         <div className={attendanceStyles.container}>
